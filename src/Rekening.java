c88d5237a2f33d39ab3d10d4dd70c816d2019fb0
Nata Inditama <natainditama.dev@gmail.com>
2025-10-13 20:48:34 +0800

    feat: add debit and credit methods to Rekening class
    Implement debitSaldo and creditSaldo methods to handle account withdrawals and deposits with validation checks. Methods throw exceptions for invalid amounts or insufficient funds.

--- src/Rekening.java
+++ src/Rekening.java
@@ -28,4 +28,28 @@ public class Rekening {
     public double getSaldo() {
         return saldo;
     }
+
+    public double debitSaldo(double jumlah) throws Exception {
+        if (jumlah <= 0) {
+            throw new Exception("Jumlah penarikan harus lebih besar dari 0");
+        }
+
+        if (jumlah > this.saldo) {
+            throw new Exception("Saldo tidak mencukupi untuk penarikan sebesar " + jumlah + ".");
+        }
+
+        double finalSaldo = this.saldo - jumlah;
+        this.saldo = finalSaldo;
+        return finalSaldo;
+    }
+
+    public double creditSaldo(double jumlah) throws Exception {
+        if (jumlah <= 0) {
+            throw new Exception("Jumlah setoran harus lebih besar dari 0");
+        }
+
+        double finalSaldo = this.saldo + jumlah;
+        this.saldo = finalSaldo;
+        return finalSaldo;
+    }
 }
