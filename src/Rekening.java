eceb6cdf50a533cf0207d72c0ef3309eccf57565
Nata Inditama <natainditama.dev@gmail.com>
2025-10-13 20:47:39 +0800

    feat: add getters and setters for Rekening class attributes

--- src/Rekening.java
+++ src/Rekening.java
@@ -8,4 +8,24 @@ public class Rekening {
         this.nomor = nomor;
         this.saldo = saldo;
     }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNomor() {
+        return nomor;
+    }
+
+    public void setNomor(String nomor) {
+        this.nomor = nomor;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
 }
